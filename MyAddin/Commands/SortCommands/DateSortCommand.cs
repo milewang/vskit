4bd6e7a0bde6544cd55a589db492603808653a93
wlahhn <wlahhn@b313cc53-264f-0410-ae0b-edc9d07c929d>
2008-06-09 07:58:37 +0000

    1. Correct command event lost bug in 'MenuManager.cs'. (Because reference is lost and collected by .NET enviroment) 2. List all Items in Solution in 'DateSortCommand.cs'

--- MyAddin/Commands/SortCommands/DateSortCommand.cs
+++ MyAddin/Commands/SortCommands/DateSortCommand.cs
@@ -1,5 +1,7 @@
 ﻿
-using System;
+using System.Text;
+using System.Windows.Forms;
+using EnvDTE;
 using EnvDTE80;
 namespace MyAddin.Commands.SortCommands
 {
@@ -12,7 +14,54 @@ namespace MyAddin.Commands.SortCommands
         }
         public override void act()
         {
-            Console.WriteLine("date sort");
+            //MessageBox.Show("Test");
+            //Project[] projects = base.Application.DTE.ActiveSolutionProjects as Project[];
+            //StringBuilder builder = new StringBuilder();
+            //foreach (SelectedItem item in base.Application.SelectedItems)
+            //{
+            //    builder.AppendLine(item.GetType() + ":" + item.Name);
+            //}
+            //MessageBox.Show(builder.ToString());
+            UIHierarchy solutionExplorer = base.Application.DTE.Windows.Item(Constants.vsext_wk_SProjectWindow).Object as UIHierarchy;
+            SelectedItems items = base.Application.SelectedItems;
+            if (null == items || items.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                StringBuilder builder = new StringBuilder();
+                UIHierarchyItems uiItems = solutionExplorer.UIHierarchyItems;
+                showNodes(uiItems.Item(1), builder, 0);
+                MessageBox.Show(builder.ToString());
+                //StringBuilder builder = new StringBuilder();
+                //foreach (SelectedItem item in items)
+                //{
+                //    SelectedItems children = item.Collection;
+                //    solutionExplorer.UIHierarchyItems
+                //}
+                //MessageBox.Show(builder.ToString());
+            }
+        }
+
+        private void showNodes(UIHierarchyItem item, StringBuilder builder, int leftSpace)
+        {
+            if (null != item)
+            {
+                builder.Append(' ', leftSpace);
+                builder.AppendLine(item.Name);
+                if (item.UIHierarchyItems.Count == 0)
+                {
+                    return;
+                }
+                else
+                {
+                    for (int i = 1; i <= item.UIHierarchyItems.Count; ++i)
+                    {
+                        showNodes(item.UIHierarchyItems.Item(i), builder, leftSpace + 1);
+                    }
+                }
+            }
         }
     }
 }
